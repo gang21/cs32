dfe8d20a3365a735fb356e98b3ca67929bb28671
Gabriella Ang <gabriellaang@Gabriellas-Air.lan>
2021-12-24 11:46:37 -0800

    test

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project1
+//
+//  Created by Gabriella Ang on 12/24/21.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
