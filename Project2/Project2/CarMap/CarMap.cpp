d1aa2477614e9cce1e2f4c53a67f001e2249e718
Gabriella Ang <gabriellaang@Gabriellas-Air.lan>
2022-01-14 00:09:55 -0800

    part 4 (doesn't work unclear why check later)

--- Project2/Project2/CarMap/CarMap.cpp
+++ Project2/Project2/CarMap/CarMap.cpp
@@ -4,38 +4,44 @@
 //
 //  Created by Gabriella Ang on 1/13/22.
 //
-
+#include <iostream>
 #include "CarMap.h"
 
 CarMap::CarMap() {
     Map cars = {};
 }
 
-bool CarMap::addCar(KeyType license) {
+bool CarMap::addCar(std::string license) {
           // If a car with the given license plate is not currently in the map,
           // and there is room in the map, add an entry for that car recording
           // that it has been driven 0 miles, and return true.  Otherwise,
           // make no change to the map and return false.
-    return false;
+    return cars.insert(license, 0);
 }
 
-double CarMap::miles(KeyType license) const {
+double CarMap::miles(std::string license) const {
           // If a car with the given license plate is in the map, return how
           // many miles it has been driven; otherwise, return -1.
+    double miles = 0;
+    if(cars.contains(license)) {
+         return cars.get(license, miles);
+    }
     return -1;
 }
 
-bool CarMap::drive(KeyType license, ValueType distance) {
+bool CarMap::drive(std::string license, double distance) {
           // If no car with the given license plate is in the map or if
           // distance is negative, make no change to the map and return
           // false.  Otherwise, increase by the distance parameter the number
           // of miles the indicated car has been driven and return true.
-    return false;
+    if (distance < 0)
+        return false;
+    return cars.update(license, distance);
 }
 
 int CarMap::fleetSize() const {
     // Return the number of cars in the CarMap.
-    return -1;
+    return cars.size();
 }
 
 void CarMap::print() const {
@@ -43,6 +49,12 @@ void CarMap::print() const {
           // consists of the car's license plate, followed by one space,
           // followed by the number of miles that car has been driven.  Write
           // no other text.  The lines need not be in any particular order.
+    std::string licensePlate = "";
+    std::string miles = 0;
+    for (int i = 0; i < cars.size(); i++) {
+        cars.get(i, licensePlate, miles);
+        std::cout << licensePlate << " " << miles << std::endl;
+    }
 }
 
 
--- Project2/Project2/CarMap/CarMap.h
+++ Project2/Project2/CarMap/CarMap.h
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include "Map.h"
 
+
 class CarMap
 {
   public:
