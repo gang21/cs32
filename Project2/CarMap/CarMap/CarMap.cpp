108aa9b7dcace46577f59b9bd2e96fa520076c30
Gabriella Ang <gabriellaang@Gabriellas-Air.lan>
2022-01-13 22:46:03 -0800

    added testMap and started #4

---
+++ Project2/CarMap/CarMap/CarMap.cpp
@@ -0,0 +1,15 @@
+//
+// CarMap.cpp
+//  CarMap
+//
+//  Created by Gabriella Ang on 1/13/22.
+//
+
+#include <iostream>
+#include "CarMap.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
