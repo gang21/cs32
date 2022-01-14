d017076da94c80310997b7da0cab279620766bd2
Gabriella Ang <gabriellaang@Gabriellas-Air.lan>
2022-01-14 12:29:31 -0800

    test functions added

--- Project2/Project2/CarMap/testCarMap.cpp
+++ Project2/Project2/CarMap/testCarMap.cpp
@@ -6,8 +6,33 @@
 //
 
 #include <stdio.h>
+#include <iostream>
 #include "CarMap.h"
 
-//int main() {
-//
-//}
+int main() {
+    CarMap jeeps;
+    assert(jeeps.fleetSize() == 0);
+    jeeps.addCar("lstrk");
+    jeeps.addCar("");
+    jeeps.addCar("123");
+    assert(!jeeps.addCar("lstrk"));
+    assert(jeeps.addCar("lStrk"));
+    jeeps.addCar("yass");
+    jeeps.addCar("zoom zoom");
+    jeeps.addCar("kachow");
+    assert(jeeps.fleetSize() == 7);
+    assert(jeeps.miles("lstrk") == 0);
+    jeeps.drive("lstrk", 100.2);
+    assert(jeeps.miles("lstrk") == 100.2);
+    jeeps.drive("kachow", -20);
+    assert(jeeps.miles("kachow") == 0);
+    jeeps.drive("123", 2145);
+    assert(jeeps.miles("123") == 2145);
+    jeeps.drive("", 23);
+    assert(jeeps.miles("") == 23);
+    jeeps.drive("zoom zoom", 649.45);
+    assert(jeeps.miles("zoom zoom") == 649.45);
+    jeeps.print();
+    
+    cerr << "all tests suceeded" << endl;
+}
