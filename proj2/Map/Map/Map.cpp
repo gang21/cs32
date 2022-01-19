4cdd551c10cc02bb711052b62f7d5e0ac4aa713e
Gabriella Ang <gabriellaang@Gabriellas-MacBook-Air.local>
2022-01-18 23:45:44 -0800

    added files for proj2

---
+++ proj2/Map/Map/Map.cpp
@@ -0,0 +1,15 @@
+//
+//  Map.cpp
+//  Map
+//
+//  Created by Gabriella Ang on 1/18/22.
+//
+
+#include "Map.h"
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "change all this later";
+    return 0;
+}
---
+++ proj2/Map/Map/Map.h
@@ -0,0 +1,12 @@
+//
+//  Map.h
+//  Map
+//
+//  Created by Gabriella Ang on 1/18/22.
+//
+
+#ifndef Map_h
+#define Map_h
+
+
+#endif /* Map_h */
