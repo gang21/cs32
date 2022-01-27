c16df9e4d596b7d605867f5ad98bf886ce38895f
Gabriella Ang <gabriellaang@Gabriellas-MacBook-Air.local>
2022-01-26 23:03:29 -0800

    problem 1

--- HW2/HW2/HW2/mazestack.cpp
+++ HW2/HW2/HW2/mazestack.cpp
@@ -27,16 +27,61 @@ bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int
     
     stack<Coord> coordStack;
     Coord start(sr, sc);
+    Coord end(er, ec);
     coordStack.push(start);
     maze[sr][sc] = '*';
     
     while (!coordStack.empty()) {
+        Coord currentCoord = coordStack.top();
+        coordStack.pop();
         
+        if (currentCoord.r() == end.r() && currentCoord.c() == end.c()) {
+            return true;
+        }
+        //west
+        if (maze[currentCoord.r()][currentCoord.c() - 1] == '.') {
+            Coord west(currentCoord.r(), currentCoord.c() - 1);
+            coordStack.push(west);
+            maze[currentCoord.r()][currentCoord.c() - 1] = '*';
+        }
+        //south
+        if (maze[currentCoord.r() + 1][currentCoord.c()] == '.') {
+            Coord south(currentCoord.r() + 1, currentCoord.c());
+            coordStack.push(south);
+            maze[currentCoord.r() + 1][currentCoord.c()] = '*';
+        }
+        //east
+        if (maze[currentCoord.r()][currentCoord.c() + 1] == '.') {
+            Coord east(currentCoord.r(), currentCoord.c() + 1);
+            coordStack.push(east);
+            maze[currentCoord.r()][currentCoord.c() + 1] = '*';
+        }
+        //north
+        if (maze[currentCoord.r() - 1][currentCoord.c()] == '.') {
+            Coord north(currentCoord.r() - 1, currentCoord.c());
+            coordStack.push(north);
+            maze[currentCoord.r() - 1][currentCoord.c()] = '*';
+        }
     }
     return false;
 }
 
 int main() {
-    
+    string maze[10] = {
+        "XXXXXXXXXX",
+        "X.X..X...X",
+        "X....XXX.X",
+        "X.XXXX.X.X",
+        "X......XXX",
+        "X.XX.X...X",
+        "X.X..X.X.X",
+        "X.X.XXXX.X",
+        "X.X...X..X",
+        "XXXXXXXXXX"
+    };
+        
+    if (pathExists(maze, 10,10, 4,6, 1,7))
+        cout << "Solvable!" << endl;
+    else
+        cout << "Out of luck!" << endl;
 }
-
