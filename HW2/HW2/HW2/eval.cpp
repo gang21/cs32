c6f821a3f415e95f9df94b7402786ee1c98acae6
Gabriella Ang <gabriellaang@Gabriellas-MacBook-Air.local>
2022-01-30 00:00:58 -0800

    return 1 function

--- HW2/HW2/HW2/eval.cpp
+++ HW2/HW2/HW2/eval.cpp
@@ -9,6 +9,7 @@
 #include "Map.h"
 #include <string>
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -32,30 +33,7 @@ int evaluate(string infix, const Map& values, string& postfix, int& result) {
     //   set to the value of the expression and the function returns 0.
     
     
-    //return 1 if it's not valid infix expression
-    if (infix == "")
-        return 1;
-    for (int i = 0; i < infix.size(); i++) {
-        if (!isalpha(infix[i])
-            && !islower(infix[i])
-            && infix[i] != '+'
-            && infix[i] != '-'
-            && infix[i] != '*'
-            && infix[i] != '/'
-            && infix[i] != '('
-            && infix[i] != ')') {
-            return 1;
-        }
-        if (infix[i] == '(') {
-            bool endParentheses = false;
-            for (int j = i; j < infix.size(); j++) {
-                if (infix[j] == ')')
-                    endParentheses = true;
-            }
-            if (!endParentheses)
-                return 1;
-        }
-    }
+    
        
     //return 2 if syntatically valid but no corresponding key/value in map
     
@@ -86,6 +64,73 @@ string clearSpaces(string infix) {
     return cleanedString;
 }
 
+bool isValidInfix(string infix) {
+    string cleanedInfix = clearSpaces(infix);
+    //return 1 if it's not valid infix expression
+    if (cleanedInfix == "")
+        return false;
+    //checking for invalid chars
+    int openParen = 0;
+    int closeParen = 0;
+    for (int i = 0; i < cleanedInfix.size(); i++) {
+        if (isalpha(cleanedInfix[i] == 0)
+            && isupper(cleanedInfix[i] != 0)
+            && cleanedInfix[i] != '+'
+            && cleanedInfix[i] != '-'
+            && cleanedInfix[i] != '*'
+            && cleanedInfix[i] != '/'
+            && cleanedInfix[i] != '('
+            && cleanedInfix[i] != ')') {
+            return false;
+        }
+        //checking for correct number of parentheses
+        if (cleanedInfix[i] == '(') {
+            openParen++;
+            //checking for expression in parentheses
+            if (i < cleanedInfix.size() - 1 && cleanedInfix[i+1] == ')')
+                return false;
+        }
+        
+        else if (cleanedInfix[i] ==')')
+            closeParen++;
+        if (closeParen > openParen)
+            return false;
+        //checking for no double operators in a row
+        if (i < cleanedInfix.size() - 1) {
+            if ((cleanedInfix[i] == '+'
+                || cleanedInfix[i] == '-'
+                || cleanedInfix[i] == '*'
+                || cleanedInfix[i] == '/')
+                && (cleanedInfix[i+1] == '+'
+                || cleanedInfix[i+1] == '-'
+                || cleanedInfix[i+1] == '*'
+                || cleanedInfix[i+1] == '/'))
+                return false;
+        }
+    }
+    if ((isalpha(cleanedInfix[0]) == 0
+        && cleanedInfix[0] != '(')
+        || cleanedInfix[cleanedInfix.size() - 1] == '+'
+        || cleanedInfix[cleanedInfix.size() - 1] == '-'
+        || cleanedInfix[cleanedInfix.size() - 1] == '*'
+        || cleanedInfix[cleanedInfix.size() - 1] == '/') {
+        return false;
+    }
+    //if infix is valid
+    return true;
+}
+
 int main() {
     cout << clearSpaces("  a +   ( d   * d)");
+    assert(isValidInfix("a+b"));
+    assert(!isValidInfix(""));
+    assert(isValidInfix("a+  b"));
+    assert(isValidInfix("a+(b)"));
+    assert(isValidInfix("a+(b"));
+    assert(!isValidInfix("a+)b"));
+    assert(isValidInfix("(a+b) * (c/d)"));
+    assert(!isValidInfix("E"));
+    assert(!isValidInfix("((a+b)c) + d"));
+    assert(isValidInfix("((a+b) * c) + d"));
+    
 }
