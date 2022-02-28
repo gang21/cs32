a3ae8904552bde0d555f6b499de386b4c07e31bd
Gabriella Ang <gabriellaang@Gabriellas-MacBook-Air.local>
2022-02-28 14:30:02 -0800

    badvector added

--- HW4/HW4/badvector.cpp
+++ HW4/HW4/badvector.cpp
@@ -1,8 +1,12 @@
-//
-//  badvector.cpp
-//  HW4
-//
-//  Created by Gabriella Ang on 2/28/22.
-//
-
-#include "badvector.hpp"
+void removeBad(vector<Movie*>& v)
+{
+    vector<Movie*>::iterator it;
+    for (it = v.begin(); it != v.end();) {
+        if ((*it)->rating() < 50) {
+            delete *it;
+            it = v.erase(it);
+        }
+        else
+            it++;
+    }
+}
