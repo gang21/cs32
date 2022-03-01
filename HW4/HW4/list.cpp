bb7ff4346cc35148157e53eb6cd08d343382f471
Gabriella Ang <gabriellaang@Gabriellas-MacBook-Air.local>
2022-02-28 18:53:40 -0800

    list.cpp

--- HW4/HW4/list.cpp
+++ HW4/HW4/list.cpp
@@ -1,8 +1,21 @@
-//
-//  list.cpp
-//  HW4
-//
-//  Created by Gabriella Ang on 2/28/22.
-//
-
-#include <stdio.h>
+void listAll(string path, const Class* c)  // two-parameter overload
+{
+    if (c == nullptr)
+        return;
+    if (path == "") {
+        cout << c->name() << endl;
+    }
+    else if (c->subclasses().size() == 0) {
+        cout << path << endl;
+        return;
+    }
+    
+    for (int i = 0; i < c->subclasses().size(); i++) {
+        if (path == "") { //root
+            listAll(c->name() + "=>" + c->subclasses()[i]->name(), c->subclasses()[i]);
+        }
+        else { // not root
+            listAll(path + "=>" + c->subclasses()[i]->name(), c->subclasses()[i]);
+        }
+    }
+}
