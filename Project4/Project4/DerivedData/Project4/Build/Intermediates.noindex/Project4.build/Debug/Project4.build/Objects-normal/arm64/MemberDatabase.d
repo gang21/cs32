c39c0ba88a9d47d61235cc65bc2ae5bb008ec0e6
Gabriella Ang <gabriellaang@Gabriellas-MacBook-Air.local>
2022-03-10 18:59:28 -0800

    idk what to do anymore

--- Project4/Project4/DerivedData/Project4/Build/Intermediates.noindex/Project4.build/Debug/Project4.build/Objects-normal/arm64/MemberDatabase.d
+++ Project4/Project4/DerivedData/Project4/Build/Intermediates.noindex/Project4.build/Debug/Project4.build/Objects-normal/arm64/MemberDatabase.d
@@ -1,6 +1,2 @@
 dependencies: \
-  /Users/gabriellaang/Desktop/cs32/Project4/Project4/Project4/MemberDatabase.cpp \
-  /Users/gabriellaang/Desktop/cs32/Project4/Project4/Project4/MemberDatabase.h \
-  /Users/gabriellaang/Desktop/cs32/Project4/Project4/Project4/PersonProfile.h \
-  /Users/gabriellaang/Desktop/cs32/Project4/Project4/Project4/RadixTree.h \
-  /Users/gabriellaang/Desktop/cs32/Unhinged/provided.h
+  /Users/gabriellaang/Desktop/cs32/Project4/Project4/Project4/MemberDatabase.cpp
--- Project4/Project4/Project4/RadixTree.h
+++ Project4/Project4/Project4/RadixTree.h
@@ -23,48 +23,6 @@ public:
     void insert(std::string key, const ValueType& value);
     ValueType* search(std::string key) const;
 private:
-//    class Node {
-//    public:
-//        Node() {
-//            prefix = nullptr;
-//            suffix = nullptr;
-//            count = 0;
-//            isLeaf = true;
-//            value = "";
-//        }
-//        ~Node() {
-//            for (int i = 0; i < 26; i++) {
-//                if (prefix[i] != nullptr) {
-//                    Node * p = prefix[i];
-//                    delete p;
-//                }
-//            }
-//        }
-//        bool addNode(Node * n, ValueType val) {
-//            for (int i = 0; i < count; i++) {
-//                if (prefix[i] == n)
-//                    return false;
-//            }
-//            prefix[count] = n;
-//            prefix[count]->setValue(val);
-//            count++;
-//            return true;
-//        }
-//        int getCount() {return count;}
-//        void setValue(ValueType key) {value = key;}
-//        ValueType getValue() {return value;}
-//        void setLeaf(bool leaf) {isLeaf = leaf;}
-//        ValueType getValAt(int i) {return prefix[i]->getValue();}
-//    private:
-//        Node * prefix[26];
-//        vector<ValueType> suffix;
-//        ValueType value;
-//        int count;
-//        bool isLeaf;
-//    };
-//    Node * m_head;
-
-    
     map<std::string, ValueType> * m_map;
 };
 
@@ -74,7 +32,7 @@ template<typename ValueType>
 RadixTree<ValueType>::RadixTree() {
     //dummy node
 //    m_head = new Node();
-    m_map = nullptr;
+//    m_map = nullptr;
 }
 
 template<typename ValueType>
@@ -92,6 +50,8 @@ void RadixTree<ValueType>::insert(std::string key, const ValueType& value) {
 
 template<typename ValueType>
 ValueType* RadixTree<ValueType>::search(std::string key) const {
+    if (m_map->size() == 0)
+        return nullptr;
     ValueType* valuecopyptr = new ValueType(m_map->find(key)->second);
     return valuecopyptr;
 }
